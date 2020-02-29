860a335546b1c0c04b83fd48767b458c1d104630
Timur Flush <flush02@tutanota.com>
2020-02-29 17:24:50 +0100

    Testing travis

--- tests/_ci/install_cmake3.sh
+++ tests/_ci/install_cmake3.sh
@@ -1,3 +1,21 @@
-sudo apt install https://launchpad.net/ubuntu/+archive/primary/+files/cmake3_3.5.1-1ubuntu3~14.04.1_amd64.deb
+#!/usr/bin/env bash
+
+# trace ERR through pipes
+set -o pipefail
+
+# trace ERR through 'time command' and other functions
+set -o errtrace
+
+# set -u : exit the script if you try to use an uninitialised variable
+set -o nounset
+
+# set -e : exit the script if any statement returns a non-true return value
+set -o errexit
+
+cd $HOME
+
+wget https://launchpad.net/ubuntu/+archive/primary/+files/cmake3_3.5.1-1ubuntu3~14.04.1_amd64.deb
+
+sudo apt install cmake3_3.5.1-1ubuntu3~14.04.1_amd64.deb
 sudo apt-get update
 sudo apt-get install cmake3
