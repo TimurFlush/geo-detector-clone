848c7ef650419990d031bd02778e03a0f52786d9
Timur Flush <flush02@tutanota.com>
2020-02-29 17:21:01 +0100

    Testing travis

--- tests/_ci/install_cmake3.sh
+++ tests/_ci/install_cmake3.sh
@@ -1,3 +1,3 @@
-sudo add-apt-repository ppa:george-edison55/cmake-3.x --yes
+sudo apt install https://launchpad.net/ubuntu/+archive/primary/+files/cmake3_3.5.1-1ubuntu3~14.04.1_amd64.deb
 sudo apt-get update
 sudo apt-get install cmake3
