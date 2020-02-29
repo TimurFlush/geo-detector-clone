505dd2c498789cbc7f3f6a9f6d24995a72800fcd
Timur Flush <flush02@tutanota.com>
2020-02-29 17:07:26 +0100

    Change way of libzip installation

---
+++ tests/_ci/install_libzip.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# trace ERR through pipes
+set -o pipefail
+
+# trace ERR through 'time command' and other functions
+set -o errtrace
+
+# set -u : exit the script if you try to use an uninitialised variable
+set -o nounset
+
+# set -e : exit the script if any statement returns a non-true return value
+set -o errexit
+
+mkdir $HOME/libzip && cd $HOME/libzip
+
+git clone https://github.com/nih-at/libzip.git && cd libzip
+mkdir build && cd build
+
+cmake3 ..
+make && make test && make install
